3ff99e711912971ab6e9a2d2ebbf26bbe0a4c2d0
david reilly <davidreilly02@gmail.com>
2024-04-29 16:57:54 +0700

    s

--- start.sh
+++ start.sh
@@ -6,5 +6,4 @@ set -o errexit
 export PATH="/opt/render/project/src/.render/chrome/opt/google/chrome:$PATH"
 
 # Start the Flask application using Gunicorn
-#gunicorn -w 4 -b 0.0.0.0:8000 main:app --timeout 120
-gunicorn --worker-class=gevent --worker-connections=1000 --workers=3 main:app --timeout 120
+gunicorn -w 4 -b 0.0.0.0:8000 main:app --timeout 120
